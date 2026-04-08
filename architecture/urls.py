e8778597e6e4f168a347981b56c1d603914852c4
david181222 <edwin.velezr@upb.edu.co>
2026-04-08 18:09:16 -0500

    Reestructura visual

--- architecture/urls.py
+++ architecture/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('videogames/', include('videogames.urls')),
+    path('', include('videogames.urls')),
 ]
